bb84e45941a7294da323a60420b19cf5d75ca624
Carlos7979 <carloscorredorwg@gmail.com>
2023-07-03 04:53:17 -0400

    add enviroment routes

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-require('dotenv').config()
 const { connect } = require('mongoose')
 const app = require('./src/app.js')
-
-const {
-    env: { PORT, MONGO_URL: url },
-    argv: [, , port = PORT || 8080],
-	pid
-} = process
+const { env: { pid, MONGO_URL: url, PORT: port, mode } } = require('./src/config')
 
 ;(async () => {
     await connect(url, { useNewUrlParser: true })
     app.listen(port, () => {
-        console.log(`Backend app listening on port ${port}\nhttp://localhost:${port}\npid: ${pid}`)
+        console.log(`Desafío 7 app listening on port ${port}`)
+        console.log('\x1b[36m%s\x1b[0m', `http://localhost:${port}`)
+		console.log('\x1b[33m%s\x1b[0m', `Mode: ${mode}`)
+        console.log(`pid: ${pid}`)
     })
 })()
